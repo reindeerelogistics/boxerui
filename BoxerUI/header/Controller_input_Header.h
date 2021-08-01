db9e7c32d77396da3d7f5cefb52a0123265d1c5e
Longqian Zhang <longqian.zhang@ontariotechu.net>
2021-08-01 16:57:29 -0400

    final

--- BoxerUI/header/Controller_input_Header.h
+++ BoxerUI/header/Controller_input_Header.h
@@ -7,9 +7,8 @@
 #include <fstream>
 #include <string>
 #include <map>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <cstdlib>
+#include "RoboCMD/RoboCMD.cpp"
 
 #include <string>
 
@@ -22,33 +21,18 @@ enum class Buttons {
 */
 namespace BoxerUI_Inputs {
 	//map<Buttons,const char*> button_map;
+RoboCMD <float,float,float,float> cmd;
 
 	std::string split(std::string target, bool dir, char delim);
-
 	std::map<std::string, std::string> getConfig();
 
 	
-	/*
-	extern void leftJoystick(const float* axes);
-	extern void rightJoystick(const float* axes);
-	extern void leftTrigger(const float* axes);
-	extern void rightTrigger(const float* axes);
-	extern void buttonA(const unsigned char* buttons);
-	extern void buttonB(const unsigned char* buttons);
-	extern void buttonX();
-	extern void buttonY();
-	extern void buttonLB();
-	extern void buttonRB();
-	extern void buttonB7();
-	extern void buttonB8();
-	extern void buttonUp();
-	extern void buttonRight();
-	extern void buttonDown();
-	extern void buttonLeft();
-	extern void input();
-	*/
+	
 	void button(const unsigned char* buttons, const char* desc, int button_val,  const char* cmd );
 	void joystick(const float* axes, const char* desc, int axes_val, const char* cmd);
 	void trigger(const float* axes, const char* desc, int axes_val, const char* cmd);
 	void input();
+	void send(const float* com,const float* data,const float* max,const float* min,const float* zero);
+	
+
 }
--- BoxerUI/src/Controller_input.cpp
+++ BoxerUI/src/Controller_input.cpp
@@ -48,7 +48,7 @@ std::string BoxerUI_Inputs::split(std::string target, bool dir, char delim) {
 		int count = 0;
 		for (int i = 0; i < target.length(); i++) {
 			if (target[i] == delim) {
-				count = i;
+				count = i+1;
 			}
 		}
 		for (int i = count; i < target.length(); i++) {
@@ -83,7 +83,8 @@ void BoxerUI_Inputs::button(const unsigned char* buttons, const char* desc, int 
 	if (getConfig().find(desc)->second != NA) {
 		if (GLFW_PRESS == buttons[button_val])
 		{
-			std::cout << cmd << getConfig().find(desc)->second << std::endl;
+			//std::cout << cmd << getConfig().find(desc)->second << std::endl;
+			send(getConfig().find(desc)->second,75,100,-100,0);
 		}
 	}
 }
@@ -94,228 +95,37 @@ void BoxerUI_Inputs::joystick(const float* axes, const char* desc, int axes_val,
 
 		if (axes[axes_val] > 0.1 || axes[axes_val] < -0.1) {
 
-			std::cout << cmd << getConfig().find(desc)->second << " Value " << -axes[axes_val] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << std::endl; // range -1-1
-
+			//std::cout << cmd << getConfig().find(desc)->second << " Value " << -axes[axes_val] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << std::endl; // range -1-1
+			send(getConfig().find(desc)->second, -axes[axes_val],1,-1,0);
 		}
+		else {
+			if (axes[axes_val] > 0.1 || axes[axes_val] < -0.1) {
+
+				//std::cout << cmd << getConfig().find(desc)->second << " Value " << axes[axes_val] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << std::endl; // range -1-1
+				send(getConfig().find(desc)->second, axes[axes_val],1,-1,0);
+
+			}
 	  }
 }
+
 
 void BoxerUI_Inputs::trigger(const float* axes, const char* desc, int axes_val, const char* cmd) {
 	std::string NA = "NA";
 	if (getConfig().find(desc)->second != NA) {
 
-		if (axes[axes_val] > -0.9) {
+		if (axes[axes_val] > -0.95) {
 
-			std::cout << cmd << getConfig().find(desc)->second << " Value " << -axes[axes_val] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << std::endl; // range -1-1
-
+			//std::cout << cmd << getConfig().find(desc)->second << " Value " << -axes[axes_val] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << std::endl; // range -1-1
+			send(getConfig().find(desc)->second, -axes[axes_val],1,-1,0);
 		}
 	}
 }
 
-
-
-/*
-void BoxerUI_Inputs::leftJoystick(const float* axes) {
-	string NA = "NA";
-	if (getConfig().find("LJ")->second != NA) {
-
-		if (axes[1] > 0.1 || axes[1] < -0.1) {
-
-			cout << "Absolute Linear velocity " << getConfig().find("LJ")->second << " Value " << -axes[1] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << endl; // range -1-1
-
-		}
-	}
+void BoxerUI_Inputs::send(const float* com,const float* data,const float* max,const float* min,const float* zero){
+	cmd.set(com,data,max,min,zero);
+	cmd.send();
 }
 
-
-void BoxerUI_Inputs::rightJoystick(const float* axes) {
-	string NA = "NA";
-	if (getConfig().find("RJ")->second != NA) {
-		if (axes[2] > 0.1 || axes[2] < -0.1) {
-			cout << "Absolute Turn " << getConfig().find("RJ")->second << " Value " << axes[2] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << endl; // range -1-1
-		}
-	}
-
-}
-
-
-void BoxerUI_Inputs::leftTrigger(const float* axes) {
-	string NA = "NA";
-	if (getConfig().find("LT")->second != NA) {
-		if (axes[4] > -0.9) {
-			cout << "Absolute Linear Aceleration " << getConfig().find("LT")->second << " Value " << axes[4] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << endl; // range 0-2
-		}
-	}
-
-}
-
-void BoxerUI_Inputs::rightTrigger(const float* axes) {
-	string NA = "NA";
-	if (getConfig().find("RT")->second != NA) {
-		if (axes[5] > -0.9) {
-			cout << "Absolute Break force " << getConfig().find("RT")->second << " Value " << axes[5] << " Max " << 1 << " Min " << -1 << " Zero " << 0 << endl;
-		}
-	}
-
-}
-
-
-void BoxerUI_Inputs::buttonA(const unsigned char* buttons) {
-	string NA = "NA";
-	if (getConfig().find("BA")->second != NA) {
-		if (GLFW_PRESS == buttons[0])
-		{
-			cout << "Multi-command " << getConfig().find("BA")->second << endl;
-		}
-	}
-
-}
-
-
-void BoxerUI_Inputs::buttonB(const unsigned char* buttons) {
-
-
-	string NA = "NA";
-	if (getConfig().find("BB")->second != NA) {
-		if (GLFW_PRESS == buttons[1])
-		{
-			cout << "Parking Break " << getConfig().find("BB")->second << endl;
-		}
-	}
-
-}
-
-void BoxerUI_Inputs::buttonX() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("BX")->second != NA) {
-		if (GLFW_PRESS == buttons[2])
-		{
-			cout << "Repeat last command " << getConfig().find("BX")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonY() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("BY")->second != NA) {
-		if (GLFW_PRESS == buttons[3])
-		{
-			cout << getConfig().find("BY")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonLB() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("LB")->second != NA) {
-		if (GLFW_PRESS == buttons[4])
-		{
-			cout << getConfig().find("LB")->second << endl;
-		}
-	}
-
-}
-
-void BoxerUI_Inputs::buttonRB() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("RB")->second != NA) {
-		if (GLFW_PRESS == buttons[5])
-		{
-			cout << getConfig().find("RB")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonB7() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("B7")->second != NA) {
-		if (GLFW_PRESS == buttons[6])
-		{
-			cout << getConfig().find("B7")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonB8() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("B8")->second != NA) {
-		if (GLFW_PRESS == buttons[7])
-		{
-			cout << getConfig().find("B8")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonUp() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("BU")->second != NA) {
-		if (GLFW_PRESS == buttons[10])
-		{
-			cout << getConfig().find("BU")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonRight() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-	string NA;
-	if (getConfig().find("BR")->second != NA) {
-		if (GLFW_PRESS == buttons[11])
-		{
-			cout << getConfig().find("BR")->second << endl;
-		}
-	}
-
-}
-
-void BoxerUI_Inputs::buttonDown() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("BD")->second != NA) {
-		if (GLFW_PRESS == buttons[12])
-		{
-			cout << getConfig().find("BD")->second << endl;
-		}
-	}
-}
-
-void BoxerUI_Inputs::buttonLeft() {
-	int buttonCount;
-	const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
-
-	string NA = "NA";
-	if (getConfig().find("BL")->second != NA) {
-		if (GLFW_PRESS == buttons[13])
-		{
-			cout << getConfig().find("BL")->second << endl;
-		}
-	}
-}
-*/
 void BoxerUI_Inputs::input() {
 	glfwWaitEventsTimeout(0.2);
 	int present = glfwJoystickPresent(GLFW_JOYSTICK_1);
@@ -336,7 +146,6 @@ void BoxerUI_Inputs::input() {
 	if (1 == present)
 	{
 		int axesCount;
-
 		int buttonCount;
 
 		const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
@@ -358,31 +167,13 @@ void BoxerUI_Inputs::input() {
 		button(buttons, "BL", 13, "button Left");
 
 
-		/*
-		buttonA(buttons);
-		buttonX();
-		buttonB(buttons);
-		buttonY();
-		buttonLB();
-		buttonRB();
-		buttonB7();
-		buttonB8();
-		buttonUp();
-		buttonRight();
-		buttonDown();
-		buttonLeft();
-		*/
+		
 		//trigger and joystick
 		joystick(axes, "LJ", 1, "Linear V");//LJ
 		joystick(axes, "LJ", 2, "Turn");//RJ
 		trigger(axes, "LT", 4, "Accerlate");//LT
 		trigger(axes, "RT", 5, "Break");//RT
-		/*
-		leftJoystick(axes);
-		rightJoystick(axes);
-		leftTrigger(axes);
-		rightTrigger(axes);
-		*/
+		
 		const char* name = glfwGetJoystickName(GLFW_JOYSTICK_1);
 		//cout << name << endl;
 	}
