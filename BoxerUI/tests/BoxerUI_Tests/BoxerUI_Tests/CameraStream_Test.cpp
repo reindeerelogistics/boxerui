092ab850a5debdafc682a75c5ccfe8dddbce7805
tiwaojo <tiwaojo@gmail.com>
2021-08-23 01:01:42 -0400

    updated tests and misc

--- BoxerUI/tests/BoxerUI_Tests/BoxerUI_Tests/CameraStream_Test.cpp
+++ BoxerUI/tests/BoxerUI_Tests/BoxerUI_Tests/CameraStream_Test.cpp
@@ -1,14 +1,16 @@
 // #include "pch.h"
 #include "CameraStream_Test.h"
+#include <iostream>
 
 TEST(TestCaseName, TestName) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
-	EXPECT_EQ(CameraStream_Test::minorTest(2), 4);
+	EXPECT_EQ(CameraStream_Test::minorTest(2), 3);
 }
 
 //This definition will ideally be with the source files
 int CameraStream_Test::minorTest(int args) {
-	return args++;
+std::cout<<"minor test args is now: "<<++args<<std::endl;
+	return args;
 }
 
